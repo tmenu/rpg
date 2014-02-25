d7dffcd897571d9482c88be26ec3ea6226d44519
Florian Martinelli <florian.martinelli@wanadoo.fr>
2014-02-25 14:53:42 +0100

    model playing_char, initial_mons, playing_mons

--- Lib/Controller/UserController.php
+++ Lib/Controller/UserController.php
@@ -26,24 +26,6 @@ class UserController extends Controller
 
 	public function signupAction()
 	{
-		$manager = Manager::getManagerOf('initial_character');
-		$entity = new Initial_character();
-
-		$entity->setHealthMax(100);
-		$entity->setHealth(100);
-		$entity->setDirection('DOWN');
-		$entity->setStrength(100);
-		$entity->setResistance(100);
-		$entity->setSpeed(100);
-		$entity->setRound(0);
-		$entity->setPosture(1);
-		$entity->setName('GUERRIER');
-		$entity->setPosition_x(0);
-		$entity->setPosition_y(1);
-		$entity->setRef('GUERRIER');
-
-
-		var_dump($manager->delete(13));
 
 		include __DIR__.'/../View/User/signup.php';
 	}
